6684e137a5bc1ec47adba9b70bf431c2a9c52842
Subodh <sohonisubodh@gmail.com>
2025-07-23 12:55:20 +0530

    Auth UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,9 @@ import type { Metadata } from "next";
 import { Nunito } from "next/font/google";
 import "./globals.css";
 import Navbar from "./components/navbar/Navbar";
+import ClientOnly from "./components/ClientOnly";
+import RegisterModal from "./components/modals/RegisterModal";
+import ToasterProvider from "./providers/ToasterProvider";
 
 
 const font = Nunito({
@@ -21,7 +24,11 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={font.className}>
-        <Navbar/>
+        <ClientOnly>
+          <ToasterProvider/>
+          <RegisterModal/>
+          <Navbar/>
+        </ClientOnly>
         {children}
       </body>
     </html>

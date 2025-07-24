a1ea22f84d232f715be45d433cdf89315c1ee4da
Subodh <sohonisubodh@gmail.com>
2025-07-24 12:14:46 +0530

    Favorite functionality

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,38 @@
+import getCurrentUser from "./actions/getCurrentUser";
+import getListings from "./actions/getListings";
+import ClientOnly from "./components/ClientOnly";
+import Container from "./components/Container";
+import EmptyState from "./components/EmptyState";
+import ListingCard from "./components/listings/ListingCard";
 
-export default function Home() {
+export default async function Home() {
+  
+  const listings = await getListings();
+  const currentUser = await getCurrentUser();
+
+  if(listings.length === 0) {
+    return (
+      <ClientOnly>
+        <EmptyState showReset/>
+      </ClientOnly>
+    );
+  }
   return (
-    <div className="text-rose-500 text-2xl">Hello</div>
+    <ClientOnly>
+      <Container>
+        <div className="pt-24 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 gap-8 2xl-grid-cols-6">
+          {/* Here you would map through the listings and render them */}
+          {listings.map((listing) => {
+            return (
+              <ListingCard
+                key={listing.id}
+                currentUser={currentUser} 
+                data={listing}
+              />
+            )
+          })}
+        </div>
+      </Container>
+    </ClientOnly>
   );
 }
--- app/types/index.ts
+++ app/types/index.ts
@@ -1,4 +1,4 @@
-import { User } from "@prisma/client";
+import { Listing, User } from "@prisma/client";
 
 export type SafeUser = Omit<
   User,
@@ -8,3 +8,5 @@ export type SafeUser = Omit<
   updatedAt: string;
   emailVerified: string | null;
 };
+
+export type safeListing = Omit<Listing, "createdAt"> & { createdAt: string };
